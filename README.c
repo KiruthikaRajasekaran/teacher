801ad7d27dc35b53edfec93116f4f9cdf7fe36e7
KiruthikaRajasekaran <krithikasadhana213@gmail.com>
2016-07-10 11:28:29 +0530

    finish

---
+++ README.c
@@ -0,0 +1,22 @@
+# teacher
+#include<stdio.h.
+#include<conio.h>
+void main()
+{
+clrscr();
+int a,b,i,j;
+c=0;
+printf("\n enter the student nums:");
+scanf("%d",&a);
+b=2a;
+for(i=0;i<b;i++)
+{
+for(j=i+1;j<b;j++)
+ {
+ printf("\n%d\n%d",i,j);
+ }
+ c++;
+ }
+ printf("the num with no pair %d",count);
+ getch();
+}
